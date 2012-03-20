eb19e6df240245afdae5120203ea8955b66eda08
Philipp <mail@philipphock.de>
2012-03-20 16:11:29 +0100

    about

--- src/de/uulm/presenter/view/Tray.java
+++ src/de/uulm/presenter/view/Tray.java
@@ -1,6 +1,8 @@
 package de.uulm.presenter.view;
 
 import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Image;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
@@ -9,16 +11,21 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import de.uulm.presenter.control.Control;
 import de.uulm.presenter.control.Main;
 import de.uulm.presenter.exceptions.TrayIconNotSupportedError;
 
-public class Tray implements ActionListener, InfoMessageListener,Observer,ProgramStateListener{
+public class Tray implements ActionListener, InfoMessageListener,Observer,ProgramStateListener, HyperlinkListener{
 	
 	private final Image trayimggreen = Toolkit.getDefaultToolkit().getImage(Tray.class.getResource("../res/TrayIcongreen.png"));
 	private final Image trayimgred = Toolkit.getDefaultToolkit().getImage(Tray.class.getResource("../res/TrayIconred.png"));
@@ -26,6 +33,7 @@ public class Tray implements ActionListener, InfoMessageListener,Observer,Progra
 	private final TrayIcon trayIcon = new TrayIcon(trayimgred,"presenter");
 	private final PopupMenu menu;
 	private final Control control;
+	private final JEditorPane aboutPane;
 	private final MenuItem restart;
 	public Tray(Control c) throws AWTException, TrayIconNotSupportedError {
 		this.control=c;
@@ -54,7 +62,11 @@ public class Tray implements ActionListener, InfoMessageListener,Observer,Progra
 			menu.add(about);
 			menu.add(exit);
 			
+			aboutPane = new JEditorPane("text/html", "<html><h1>Presenter App</h1><p>This is the Server Component of the Presenter App for Java ME<br><p>This Application was part of the Appventure Camp by Nokia</p><br>by<br>&nbsp;Karoline Blendinger<br>&nbsp;Philipp Hock</p><br><br><a href=\"https://github.com/HackCamp2012\">follow us on GitHub</a></html>");
 			
+			aboutPane.addHyperlinkListener(this);
+			aboutPane.setEditable(false);
+			aboutPane.setBackground(new Color(1, true));
 			trayIcon.setPopupMenu(menu);
 			
 		}else{
@@ -69,9 +81,14 @@ public class Tray implements ActionListener, InfoMessageListener,Observer,Progra
 			control.exit();
 		}else if (event.getActionCommand().equals("tray_listen")){
 			control.listen();
+		}else if (event.getActionCommand().equals("tray_about")){
+			
+			JOptionPane.showMessageDialog(null,aboutPane, "Presenter - About", JOptionPane.PLAIN_MESSAGE, null);
 		}
 		
 	}
+	
+	
 	
 	@Override
 	public void showInfoMessage(String msg, String title){
@@ -126,6 +143,23 @@ public class Tray implements ActionListener, InfoMessageListener,Observer,Progra
 	public void serverConnected() {
 		trayIcon.setImage(trayimggreen);
 		trayIcon.setToolTip("Presenter - connected");
+		
+	}
+
+
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+		System.out.println(e);
+		try {
+			Desktop.getDesktop().browse(e.getURL().toURI());
+		} catch (IOException e1) {
+
+			e1.printStackTrace();
+		} catch (URISyntaxException e1) {
+
+			e1.printStackTrace();
+		}
+		
 		
 	}
 	
